d2bbb404837ab0a49f43bc7ada9e5f92ae5a0bb7
Ruize Ma <yibagaozi@outlook.com>
2025-03-29 02:32:39 +0800

    SpringBoot project build
    Build SpringBoot project combining JavaFX

---
+++ src/main/java/org/softeng/group77/pennyplanner/PennyPlannerApplication.java
@@ -0,0 +1,13 @@
+package org.softeng.group77.pennyplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PennyPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PennyPlannerApplication.class, args);
+	}
+
+}
