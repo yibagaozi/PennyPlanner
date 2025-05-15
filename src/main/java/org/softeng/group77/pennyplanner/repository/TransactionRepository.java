d7e9c682b37f1f403cf24043db7d73ef9195bb9a
xxxxxxxxx12345 <jp2022213430@qmul.ac.uk>
2025-05-15 13:57:43 +0800

    Update TransactionRepository.java

--- src/main/java/org/softeng/group77/pennyplanner/repository/TransactionRepository.java
+++ src/main/java/org/softeng/group77/pennyplanner/repository/TransactionRepository.java
@@ -3,6 +3,7 @@ package org.softeng.group77.pennyplanner.repository;
 import org.softeng.group77.pennyplanner.model.Transaction;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,5 +18,10 @@ public interface TransactionRepository {
     List<Transaction> findByUserId(String userId) throws IOException;
 
     List<Transaction> findByUserIdAndDescription(String userId, String description) throws IOException;
+     List<Transaction> findByUserIdAndTransactionDateTimeBetween(
+            String userId,
+            LocalDateTime start,
+            LocalDateTime end
+    );
 
 }
